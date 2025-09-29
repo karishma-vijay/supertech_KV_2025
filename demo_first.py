39aaed9c880cf462ddb6ffe9ed0cdd4985a44d02
Karishma <karishmavijay21@gmail.com>
2025-09-29 14:02:20 +0100

    CREATED: our first demo script

---
+++ demo_first.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+# Author: KVijay
+# Description: This script will demo
+"""
+    Docstring:
+"""
+#! /usr/bin/env python3
+"""
+this is a multi line comment
+slay
+
+"""
+
+# name = "karishma"
+name = input("enter name: ")
+print("My name is ", name, sep=" ")
+print("my name is " + name)
+
+import math
+print("Cosine of 0.5 =", math.cos(0.5))
+
+import random
+print("my lucky number is", random.randint(1, 50))
+
+print("done")
